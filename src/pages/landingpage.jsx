84ecf700f7d67e74d5f404d2b969a622d922dc46
Daniel Montesclaros <147793265+constantine2003@users.noreply.github.com>
2025-03-21 20:17:33 +0800

    -Fonts Added
    im getting lazy too cold. will continue the next day!!

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -1,9 +1,30 @@
-function LandingPage(){
-    return(
-        <div>
-            <p style={{}}>Hello World</p>
+import styles from "../styles/landingpage.module.css";  // Correct import
+
+function LandingPage() {
+    return (
+        <div className={styles.container}>
+            {/*Top bar*/}
+            <div className={styles.sidebar}> 
+
+            </div>
+
+            <div className={styles.profilecontent}>
+                <div className={styles.firsthalf}>
+                    <div className={styles.wrap}>
+                        <h1 className={styles.first}>Hi,</h1>
+                        <h1 className={styles.second}>I'm Joy Arenas</h1>
+                        <h1 className={styles.second}>a <span className={styles.fourth}>Mathematician</span></h1>
+                        <h3 className={styles.third}>BS MATH / DOST SCHOLAR / UP GRADUATE</h3>
+                        
+                    </div>
+                </div>
+                <div className={styles.secondhalf}>
+                    
+                </div>
+            </div>
         </div>
-    )
+        
+    );
 }
 
 export default LandingPage;
